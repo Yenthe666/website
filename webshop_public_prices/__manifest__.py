68634bc2dc3cc3876fbcd56e705f8bb58752c9c9
root <yenthevg@gmail.com>
2022-03-11 14:43:03 +0100

    [ADD] webshop_public_prices: allow hiding product price/add to cart

---
+++ webshop_public_prices/__manifest__.py
@@ -0,0 +1,20 @@
+{
+	'name': 'Webshop Public Prices',
+	'category': 'Website',
+	'version': '15.0.1.0',
+	'author': 'Mainframe Monkey',
+	'website': 'https://www.mainframemonkey.com',
+	'summary': 'Webshop Public Prices',
+	'description': """
+		Allow configuring to hide or show prices/add to cart for public users
+	""",
+	'depends': [
+		'website',
+		'website_sale',
+	],
+	'data': [
+		'views/res_config_settings_views.xml',
+		'views/website_sale_template.xml',
+	],
+	'installable': True,
+}
