78351b7e70fbd396bd07ad5fa78a4d41cc3ea600
Ward M <ward.marissen@gmail.com>
2022-09-01 09:56:20 +0200

    [ADD] webshop_quick_sell_product_accessory_public_prices: Hide prices on accessories

---
+++ webshop_quick_sell_product_accessory_public_prices/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+{
+	'name': 'Website Sale Product Accessory Public Prices',
+	'category': 'Website',
+	'version': '15.0.1.0.0',
+	'author': 'Mainframe Monkey',
+	'website': 'https://www.mainframemonkey.com',
+	'summary': 'Website Quick Sell Product accessories with public prices',
+	'description': """Website Quick Sell Product accessories with public prices""",
+	'depends': [
+		'webshop_public_prices',
+		'webshop_quick_sell_product_accessory',
+	],
+	'data': [
+		'views/optional_product_view.xml',
+	],
+	'license': 'LGPL-3',
+}
