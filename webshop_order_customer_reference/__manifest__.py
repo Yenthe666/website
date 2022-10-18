c21a65b18c48ad4c33fb6da3958e33e345c832d0
Yenthe Van Ginneken <yenthevg@gmail.com>
2022-10-18 14:45:01 +0200

    [IMP] webshop_order_customer_reference: correct version

--- webshop_order_customer_reference/__manifest__.py
+++ webshop_order_customer_reference/__manifest__.py
@@ -17,7 +17,7 @@
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Uncategorized',
-    'version': '14.0.0.0.1',
+    'version': '15.0.0.0.1',
 
     # any module necessary for this one to work correctly
     'depends': ['website_sale'],
